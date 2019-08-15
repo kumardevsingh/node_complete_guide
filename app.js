c83390eaeaa25f5311f81dc1aa2ff17748cfac60
kumardevsingh <kumardevsingh@gmail.com>
2019-08-15 23:41:44 +0530

    installing express

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const http = require('http');
-const routes = require('./routes');
+//const routes = require('./routes');
+
+const express =  require('express');
+
+const app = express();
+
+//const server = http.createServer(routes);
+
+app.use((req,res,next) =>{
+    console.log('in middleware');
+    res.send(`<h1> From express</h1>`)
+    next();
+})
+
+app.use((req,res,next) =>{
+    console.log('in another  middleware');
+   // res.send(`<h1> From express -2</h1>`)
+    //next();
+})
 
 
-const server = http.createServer(routes);
-
-server.listen(3000);
+//const server = http.createServer(app);
+//server.listen(3000);
+app.listen(3000);
